90d05401c8a91b04c9e02213f5a90dd149adf088
claudio <claudio.goncalves.filho@outlook.com>
2018-12-10 22:09:50 -0200

    search field added

--- clientes/views.py
+++ clientes/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Concat
+from django.db.models import Value
+
 
 
 # Create your views here.
 
 @login_required
 def person_list(request):
-    persons = Person.objects.all()
+
+    busca = request.GET.get('pesquisa', None)
+
+    if busca:
+        query = Person.objects.all()
+        query_fullname = Person.objects.annotate(fullname=Concat('first_name', Value(' '), 'last_name'))
+        persons = query_fullname.filter(fullname__icontains=busca)
+    else:
+        persons = Person.objects.all()
+
     return render(request, "person.html", {'persons':persons})
 
 @login_required
